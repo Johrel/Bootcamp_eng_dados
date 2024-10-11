4414823f2abb2eaf8a811d7a9ead9791bbf89db8
Jonfres <jhouers@gmail.com>
2024-10-11 19:20:42 -0300

    Update

--- dags/dag_airbyte.py
+++ dags/dag_airbyte.py
@@ -15,14 +15,14 @@ def running_airbyte():
     start_airbyte_sync = SimpleHttpOperator(
         task_id='start_airbyte_sync',
         http_conn_id='airbyte_default',
-        endpoint='api/v1/connections/sync',  # api/v1/connections/sync Endpoint correto para disparar a sincronização
+        endpoint='/v1/jobs',  # api/v1/connections/sync Endpoint correto para disparar a sincronização
         method='POST',
         headers={"Content-Type": "application/json", 
                  "User-Agent":"fake-useragent", 
                  "Accept":"application/json",
                  "Authorization": API_KEY},
         data=json.dumps({"connectionId": AIRBYTE_CONNECTION_ID, "jobType":"sync"}),  # Assegure que o connectionId está correto
-        response_check=lambda response: response.json()['status'] == 'running'
+         response_check=lambda response: response.json().get('status') == 'running'
     )
 
     start_airbyte_sync
