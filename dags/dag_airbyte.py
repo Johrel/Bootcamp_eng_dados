431d476e82fd554a85d1f4891cbd2dad2b75f662
Jonfres <jhouers@gmail.com>
2024-10-12 11:14:30 -0300

    Update

--- dags/dag_airbyte.py
+++ dags/dag_airbyte.py
@@ -3,10 +3,33 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.models import Variable
 import json
+import requests
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
 AIRBYTE_CONNECTION_ID = Variable.get("AIRBYTE_GOOGLE_POSTGRES_CONNECTION_ID")
-API_KEY = f'Bearer {Variable.get("AIRBYTE_API_TOKEN")}'
+load_dotenv()
+
+client_id = os.getenv("AIRBYTE_CLIENT_ID")
+client_secret = os.getenv("AIRBYTE_CLIENT_SECRET")
+
+def get_new_jwt():
+# Carregar variáveis de ambiente do arquivo .env
+    
+
+    # Implemente a lógica para obter um novo token JWT aqui
+    response = requests.post('https://api.airbyte.com/api/v1/applications/token', 
+                             data=json.dumps({"client_id": client_id,
+                                               "client_secret": client_secret}),
+                                                headers={"Content-Type": "application/json"})
+    if response.status_code == 200:
+        return response.json().get('token')  # Supondo que o token é retornado assim
+    else:
+        raise Exception("Erro ao obter novo token JWT")
+
+# Use a função para obter um novo token antes de fazer a chamada que falhou
+API_KEY = get_new_jwt()
 
 
 @dag(start_date=datetime(2024, 10, 11), schedule_interval="@daily", catchup=False)
